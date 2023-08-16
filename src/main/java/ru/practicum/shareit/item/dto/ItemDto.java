17bb0eec1a1b9752be557f8de231ff8cdb07ed37
Vlad <ma1ngeek@yandex.ru>
2023-08-16 23:28:09 +0300

    Ready2

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,33 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+    private long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+
+
+    public ItemDto(String name, String description, boolean available) {
+        this.name = name;
+        this.description = description;
+        this.available = available;
+    }
 }
